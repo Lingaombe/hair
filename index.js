854e43cef599440cceac2e35d14199bf1346cb61
Lingaombe <mngolihannah@gmail.com>
2025-07-12 16:17:24 +0530

    added env and gitignore

---
+++ index.js
@@ -0,0 +1,10 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const token = process.env.Pinterest_Token;
+
+//process which radio set is picked
+//define functions for each set pinterest API get board and randomly select pin
+//onclick call req function
+//add link to pin ref pinterest
